59113e3854aa208f49cfa82aa103008008a88d8a
shantanu <shantanuvelnati@gmail.com>
2024-01-30 13:49:00 -0500

    Added Firabase support that has cloud functions

--- Petsistant/ContentView.swift
+++ Petsistant/ContentView.swift
@@ -1,45 +1,61 @@
-//
-//  ContentView.swift
-//  Petsistant
-//
-//  Created by Shantanu Velnati on 1/28/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @State private var currentQuote = "Tap the lion to get a motivational quote! 🚀"
     @State private var isJumping = false
+
     var body: some View {
-    ZStack{
-        Color.black.edgesIgnoringSafeArea(.all)
-        VStack {
-            Text("🦁")
-                .font(.system(size: 100))
-                .offset(y: isJumping ? -20 : 0)
-                .onTapGesture {
-                    withAnimation { self.isJumping = true}
-                    DispatchQueue.main.async {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("🦁")
+                    .font(.system(size: 100))
+                    .offset(y: isJumping ? -20 : 0)
+                    .onTapGesture {
+                        withAnimation { self.isJumping = true }
                         DispatchQueue.main.asyncAfter(deadline: .now() + 0.35) {
                             withAnimation {
                                 self.isJumping = false
+                                self.fetchQuote()
                             }
                         }
                     }
-                    
-                }
                                     
-            Text(currentQuote)
-                .font(.headline)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-                .padding()
+                Text(currentQuote)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            }
         }
-        
+    }
+
+    func fetchQuote() {
+        guard let url = URL(string: "https://chat-cb5srbnobq-uc.a.run.app") else {
+            print("Invalid URL")
+            return
         }
+
+        let request = URLRequest(url: url)
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.currentQuote = decodedResponse.reply
+                    }
+                }
+            }
+        }.resume()
+    }
+
+    struct Response: Codable {
+        var reply: String
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
